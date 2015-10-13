ea5e2c87a2b7ca31e06d0219404db2c36a4713ba
Nick Huey <huey17@up.edu>
2015-10-13 11:13:05 -0700

    Added modular code

--- public/ui.js
+++ public/ui.js
@@ -5,63 +5,71 @@
  *
  */
 
-// fetch helps get specific inputs displayed by year 
-var fetch = function() {
-	var yearFetcher = document.getElementById('year');
-	var value = yearFetcher.value;
-	console.log(value);
-	
-	//note to self. it is viz, not vis (this is what caused me so many delays)
-	vizController(value);
+var librs = librs || {};
+librs.ui = {};
+librs.ui = function() {
+
+	// fetch helps get specific inputs displayed by year
+	var fetch = function() {
+		var yearFetcher = document.getElementById('year');
+		var value = yearFetcher.value;
+		console.log(value);
+
+		//note to self. it is viz, not vis (this is what caused me so many delays)
+		vizController(value);
+	};
+
+	var counter = 1;
+
+	var toggle = function() {
+
+		console.log('About!');
+
+		// Grab the html element with the ID “about”
+		var el = document.getElementById('about');
+
+		if (counter % 2) {
+			console.log('TOGGLE ON');
+			// Add the class “show” to the element.
+			addClass(el, 'show');
+		} else {
+			console.log('TOGGLE OFF');
+			removeClass(el, 'show');
+		}
+		counter = counter + 1;
+
+	};
+
+	var initialize = function() {
+
+		console.log('Initialize!');
+
+		// Grab the 'About' button element, identified by the
+		// 'about-btn' id.
+		var button = document.getElementById('about-btn');
+
+		// From this point forward, when the button is clicked, the
+		// toggle function shall be invoked.
+		button.onclick = toggle;
+
+		// Grab the 'Submit' button element, identified by the
+		// 'submit-btn' id.
+		submitButton = document.getElementById('submit-btn');
+
+		// From this point forward, when the button is clicked, the
+		// fetch function shall be invoked.
+		submitButton.onclick = fetch;
+	};
+
+	// When this file is included at the bottom of the page,
+	// the js is loaded after the DOM is loaded.  It is a
+	// good time to initialize the UI elements in the page.
+	initialize();
+
 };
+// end module
 
+// Invoke module. After invocation, the module’s code is now added to
+// the namespace and is accessible through the librs object.
+librs.ui();
 
-
-
-var counter = 1;
-
-var toggle = function() {
-
-	console.log('About!');
-
-	// Grab the html element with the ID “about”
-	var el = document.getElementById('about');
-
-	if (counter % 2) {
-		console.log('TOGGLE ON');
-		// Add the class “show” to the element.
-		addClass(el, 'show');
-	} else {
-		console.log('TOGGLE OFF');
-		removeClass(el, 'show');
-	}
-	counter = counter + 1;
-
-};
-
-var initialize = function() {
-
-	console.log('Initialize!');
-
-	// Grab the 'About' button element, identified by the
-	// 'about-btn' id.
-	var button = document.getElementById('about-btn');
-
-	// From this point forward, when the button is clicked, the
-	// toggle function shall be invoked.
-	button.onclick = toggle;
-	
-	// Grab the 'Submit' button element, identified by the
-	// 'submit-btn' id.
-	submitButton = document.getElementById('submit-btn');
-
-	// From this point forward, when the button is clicked, the
-	// fetch function shall be invoked.
-	submitButton.onclick = fetch;
-};
-
-
-// When this file is included at the bottom of the page,
-// the js is loaded after the DOM is loaded.  It is a
-// good time to initialize the UI elements in the page.
-initialize();
--- public/utility.js
+++ public/utility.js
@@ -1,68 +1,90 @@
-/**                                                                                                                
+/**
  * utility.js
- *                                                                                                                 
+ *
  * Defines generic helper functionality for instrumenting the
  * user-interface.  Based on the tutorials at:
  *
  * http://www.kirupa.com/snippets/add_class_and_remove_class_snippet.htm
  *    and
  * http://sonnyt.com/javascript-check-if-element-has-class/
- *                                                                                                                 
+ *
  */
 
+// Extend namespace
+var librs = librs || {};
+librs.utility = {};
 
-// hasClass
-//
-// Given an element and a className, return true if the element has
-// the class className.
-var hasClass = function(element, className) {
+// The intention of this approach is to be framework-agnostic
+// and to avoid namespace pollution by encapsulating the contents of
+// this module in a function:
+librs.utility = function() {
 
-    return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
+	// *******************************************************************
+	// Let’s put methods local to this module here.
+	// *******************************************************************
+
+	// *******************************************************************
+	// Let’s put methods exposed by this module here.
+	// *******************************************************************
+
+	// hasClass
+	//
+	// Given an element and a className, return true if the element has
+	// the class className.
+	librs.utility.hasClass = function(element, className) {
+
+		return element.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(element.className);
+	};
+
+	// addClass
+	//
+	// Given an element and a classToAdd, add the class to the element
+	// if it doesn't already have it.
+	librs.utility.addClass = function(element, classToAdd) {
+
+		var currentClassValue = element.className;
+
+		if (currentClassValue.indexOf(classToAdd) == -1) {
+			if ((currentClassValue == null) || (currentClassValue === "")) {
+				element.className = classToAdd;
+			} else {
+				element.className += " " + classToAdd;
+			}
+		}
+	};
+
+	// removeClass
+	//
+	// Given an element and a classToRemove, remove the class
+	// from the element if it is present.
+	librs.utility.removeClass = function(element, classToRemove) {
+		var currentClassValue = element.className;
+
+		// removing a class value when there is more than one class
+		// value present and the class you want to remove is not the
+		// first one
+		if (currentClassValue.indexOf(" " + classToRemove) != -1) {
+			element.className = element.className.replace(" " + classToRemove, "");
+			return;
+		}
+
+		// removing the first class value when there is more than one
+		// class value present
+		if (currentClassValue.indexOf(classToRemove + " ") != -1) {
+			element.className = element.className.replace(classToRemove + " ", "");
+			return;
+		}
+
+		// removing the first class value when there is only one class
+		// value present
+		if (currentClassValue.indexOf(classToRemove) != -1) {
+			element.className = element.className.replace(classToRemove, "");
+			return;
+		}
+	};
+
 };
+// end module
 
-// addClass
-//
-// Given an element and a classToAdd, add the class to the element
-// if it doesn't already have it.
-var addClass = function(element, classToAdd) {
-
-    var currentClassValue = element.className;
-
-    if (currentClassValue.indexOf(classToAdd) == -1) {
-        if ((currentClassValue == null) || (currentClassValue === "")) {
-            element.className = classToAdd;
-        } else {
-            element.className += " " + classToAdd;
-        }
-    }
-};
-
-// removeClass
-//
-// Given an element and a classToRemove, remove the class
-// from the element if it is present.
-var removeClass = function(element, classToRemove) {
-    var currentClassValue = element.className;
-    
-    // removing a class value when there is more than one class
-    // value present and the class you want to remove is not the
-    // first one
-    if (currentClassValue.indexOf(" " + classToRemove) != -1) {
-	element.className = element.className.replace(" " + classToRemove, "");
-	return;
-    }
-    
-    // removing the first class value when there is more than one
-    // class value present
-    if (currentClassValue.indexOf(classToRemove + " ") != -1) {
-	element.className = element.className.replace(classToRemove + " ", "");
-	return;
-    }
-    
-    // removing the first class value when there is only one class
-    // value present
-    if (currentClassValue.indexOf(classToRemove) != -1) {
-	element.className = element.className.replace(classToRemove, "");
-	return;
-    }
-};
+// Invoke module.
+librs.utility();
